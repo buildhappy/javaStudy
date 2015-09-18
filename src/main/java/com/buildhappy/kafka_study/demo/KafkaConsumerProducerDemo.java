37ef970eb42d3ba1f34cc7060926f94dc0806b86
caijianfu <caijianfu02@meituan.com>
2015-09-18 17:41:35 +0800

    update kafka case

--- src/main/java/com/buildhappy/kafka_study/demo/KafkaConsumerProducerDemo.java
+++ src/main/java/com/buildhappy/kafka_study/demo/KafkaConsumerProducerDemo.java
@@ -1,5 +1,9 @@
 package com.buildhappy.kafka_study.demo;
 
+import com.buildhappy.kafka_study.KafkaProperties;
+import com.buildhappy.kafka_study.consumer.KafkaConsumer;
+import com.buildhappy.kafka_study.producer.KafkaProducer;
+
 public class KafkaConsumerProducerDemo{
     public static void main(String[] args){
         KafkaProducer producerThread = new KafkaProducer(KafkaProperties.topic);
